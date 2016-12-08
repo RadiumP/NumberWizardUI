7e60db913014c2d73f6146390f29ebec8fa3ddc9
RadiumP <chenyan5@asu.edu>
2016-12-08 11:36:10 -0700

    v0.2

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,16 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public void LoadNewLevel(string name)
     {
         Debug.Log("Level load requested" + name);
+        //Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
     }
 
     public void QuitRequest()
     {
         Debug.Log("Quit requested");
+        Application.Quit();//Limitations: Works great for pc and consoles but not debug and Web/Mobile, so not good
+    }
+
+    public void ReturnToMain()
+    {
+        string name = "Start";
+        SceneManager.LoadScene(name);       
     }
 
 }
